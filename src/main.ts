6d7ec6c8f1784d32662ac9eca76536d8d01575f9
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-03-17 04:51:41 -0300

    agrego a cors conlara.onrender.com

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, SwaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  // Habilitar CORS
+  // Habilitar CORS con múltiples orígenes permitidos
   app.enableCors({
-    origin: 'http://localhost:5173', // Cambia esto según la URL de tu frontend
+    origin: ['http://localhost:5173', 'https://conlara.onrender.com'], // Agrega todos los dominios permitidos
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
@@ -36,6 +36,45 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import {
+//   loggerGlobal,
+//   LoggerMiddleware,
+// } from './middlewares/logger.middleware';
+// import { ValidationPipe } from '@nestjs/common';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   // Configuración de Swagger
+//   const SwaggerConfig = new DocumentBuilder()
+//     .setTitle('E-commerce')
+//     .setDescription('API para e-commerce')
+//     .setVersion('1.0')
+//     .addBearerAuth()
+//     .build();
+//   const document = SwaggerModule.createDocument(app, SwaggerConfig);
+//   SwaggerModule.setup('api', app, document);
+
+//   // Habilitar CORS
+//   app.enableCors({
+//     origin: 'http://localhost:5173', // Cambia esto según la URL de tu frontend
+//     methods: 'GET,POST,PUT,DELETE,OPTIONS',
+//     allowedHeaders: 'Content-Type, Authorization',
+//   });
+
+//   // Middleware y Validación Global
+//   app.use(loggerGlobal);
+//   const loggerMiddleware = new LoggerMiddleware();
+//   app.use(loggerMiddleware.use);
+//   app.useGlobalPipes(new ValidationPipe());
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
 
 // import { NestFactory } from '@nestjs/core';
 // import { AppModule } from './app.module';
