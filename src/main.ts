0f6954e52f7a9b3c3be84e8a094d6468c75919d3
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-03-17 01:12:52 -0300

    cors instalado

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración de Swagger
   const SwaggerConfig = new DocumentBuilder()
     .setTitle('E-commerce')
     .setDescription('API para e-commerce')
@@ -17,10 +19,47 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, SwaggerConfig);
   SwaggerModule.setup('api', app, document);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Cambia esto según la URL de tu frontend
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  // Middleware y Validación Global
   app.use(loggerGlobal);
   const loggerMiddleware = new LoggerMiddleware();
   app.use(loggerMiddleware.use);
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import {
+//   loggerGlobal,
+//   LoggerMiddleware,
+// } from './middlewares/logger.middleware';
+// import { ValidationPipe } from '@nestjs/common';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const SwaggerConfig = new DocumentBuilder()
+//     .setTitle('E-commerce')
+//     .setDescription('API para e-commerce')
+//     .setVersion('1.0')
+//     .addBearerAuth()
+//     .build();
+//   const document = SwaggerModule.createDocument(app, SwaggerConfig);
+//   SwaggerModule.setup('api', app, document);
+//   app.use(loggerGlobal);
+//   const loggerMiddleware = new LoggerMiddleware();
+//   app.use(loggerMiddleware.use);
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
