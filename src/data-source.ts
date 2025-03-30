2f4b0be6b4e0c47a486634ca6220468e2cb989d5
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-03-29 21:16:47 -0300

    hay dios

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,13 +3,13 @@ import { User } from './users/user.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgresa',
-  password: '1234',
-  database: 'ecommerce',
+  host: process.env.DB_HOST || 'localhost', // Configurable desde variables de entorno
+  port: parseInt(process.env.DB_PORT, 10) || 5432, // Configurable desde variables de entorno
+  username: process.env.DB_USERNAME || 'postgresa', // Configurable desde variables de entorno
+  password: process.env.DB_PASSWORD || '1234', // Configurable desde variables de entorno
+  database: process.env.DB_NAME || 'ecommerce', // Configurable desde variables de entorno
   entities: [User],
-  synchronize: false,
-  logging: true,
-  migrations: ['src/migrations/*.ts'], // Asegúrate de tener la ruta correcta para las migraciones
+  synchronize: false, // Asegúrate de usar migraciones en lugar de sincronización automática en producción
+  logging: process.env.NODE_ENV !== 'production', // Activar logs solo en desarrollo
+  migrations: ['dist/migrations/*.js'], // Asegúrate de compilar migraciones en producción
 });
