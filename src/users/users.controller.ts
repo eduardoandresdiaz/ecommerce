32bb8e87518e2ea6c3f7b9758eaf38fbc8d658b8
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-04-01 02:12:32 -0300

    creacion end point buscar por email

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,18 +13,16 @@ import {
 import { UserService } from './users.service';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { AuthGuard } from '../guards/auth.guard';
-import { RolesGuard } from '../guards/roles.guard';
-import { Roles } from '../decorators/roles/roles.decorator';
-import { Role } from '../enum/roles.enum';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UserService) {}
+
   @ApiBearerAuth()
   @Get()
-  @Roles(Role.ADMIN)
-  @UseGuards(AuthGuard, RolesGuard)
+  @HttpCode(200)
+  @UseGuards(AuthGuard)
   getUsers(@Query('page') page: number, @Query('limit') limit: number) {
     return this.userService.getUsers(page, limit);
   }
@@ -37,8 +35,15 @@ export class UsersController {
   }
 
   @HttpCode(200)
+  @Get('email/:email')
+  @UseGuards(AuthGuard)
+  getUserByEmail(@Param('email') email: string) {
+    return this.userService.getUserByEmail(email);
+  }
+
+  @HttpCode(200)
   @Put(':id')
-  //@UseGuards(AuthGuard)
+  @UseGuards(AuthGuard)
   updateUser(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -48,9 +53,66 @@ export class UsersController {
 
   @HttpCode(200)
   @Delete(':id')
-  //@UseGuards(AuthGuard)
+  @UseGuards(AuthGuard)
   deleteUser(@Param('id', ParseUUIDPipe) id: string) {
     console.log('users controller delete');
     return this.userService.deleteUser(id);
   }
 }
+
+// import {
+//   Body,
+//   Controller,
+//   Delete,
+//   Get,
+//   HttpCode,
+//   Param,
+//   ParseUUIDPipe,
+//   Put,
+//   Query,
+//   UseGuards,
+// } from '@nestjs/common';
+// import { UserService } from './users.service';
+// import { UpdateUserDto } from '../dto/update-user.dto';
+// import { AuthGuard } from '../guards/auth.guard';
+// import { RolesGuard } from '../guards/roles.guard';
+// import { Roles } from '../decorators/roles/roles.decorator';
+// import { Role } from '../enum/roles.enum';
+// import { ApiBearerAuth } from '@nestjs/swagger';
+
+// @Controller('users')
+// export class UsersController {
+//   constructor(private readonly userService: UserService) {}
+//   @ApiBearerAuth()
+//   @Get()
+//   @Roles(Role.ADMIN)
+//   @UseGuards(AuthGuard, RolesGuard)
+//   getUsers(@Query('page') page: number, @Query('limit') limit: number) {
+//     return this.userService.getUsers(page, limit);
+//   }
+
+//   @HttpCode(200)
+//   @Get(':id')
+//   @UseGuards(AuthGuard)
+//   getUserById(@Param('id', ParseUUIDPipe) id: string) {
+//     return this.userService.getUserById(id);
+//   }
+
+//   @HttpCode(200)
+//   @Put(':id')
+//   //@UseGuards(AuthGuard)
+//   updateUser(
+//     @Param('id', ParseUUIDPipe) id: string,
+//     @Body() updateUserDto: UpdateUserDto,
+//   ) {
+//     return this.userService.updateUser(String(id), updateUserDto);
+//   }
+
+//   @HttpCode(200)
+//   @Delete(':id')
+//   //@UseGuards(AuthGuard)
+//   deleteUser(@Param('id', ParseUUIDPipe) id: string) {
+//     console.log('users controller delete');
+//     return this.userService.deleteUser(id);
+//   }
+// }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from './users.repository';
 import { User } from './user.entity';
-import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -15,6 +14,16 @@ export class UserService {
     return this.userRepository.getUserById(id);
   }
 
+  async getUserByEmail(email: string): Promise<User> {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) {
+      throw new NotFoundException(
+        'Usuario no encontrado con el correo especificado',
+      );
+    }
+    return user;
+  }
+
   createUser(
     user: Omit<User, 'id'>,
   ): Promise<Omit<User, 'password' | 'isAdmin'>> {
@@ -23,7 +32,7 @@ export class UserService {
 
   async updateUser(
     id: string,
-    updateUserDto: UpdateUserDto,
+    updateUserDto: Partial<User>,
   ): Promise<Omit<User, 'password'>> {
     const user = await this.userRepository.getUserById(id);
 
@@ -43,3 +52,49 @@ export class UserService {
     return this.userRepository.deleteUser(id);
   }
 }
+
+// import { Injectable } from '@nestjs/common';
+// import { UserRepository } from './users.repository';
+// import { User } from './user.entity';
+// import { UpdateUserDto } from '../dto/update-user.dto';
+
+// @Injectable()
+// export class UserService {
+//   constructor(private userRepository: UserRepository) {}
+
+//   getUsers(page: number, limit: number) {
+//     return this.userRepository.getUsers(page, limit);
+//   }
+
+//   getUserById(id: string) {
+//     return this.userRepository.getUserById(id);
+//   }
+
+//   createUser(
+//     user: Omit<User, 'id'>,
+//   ): Promise<Omit<User, 'password' | 'isAdmin'>> {
+//     return this.userRepository.createUser(user);
+//   }
+
+//   async updateUser(
+//     id: string,
+//     updateUserDto: UpdateUserDto,
+//   ): Promise<Omit<User, 'password'>> {
+//     const user = await this.userRepository.getUserById(id);
+
+//     for (const key in updateUserDto) {
+//       if (Object.prototype.hasOwnProperty.call(updateUserDto, key)) {
+//         user[key] = updateUserDto[key];
+//       }
+//     }
+
+//     const updatedUser = await this.userRepository.save(user);
+//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//     const { password, ...userWithoutPassword } = updatedUser;
+//     return userWithoutPassword;
+//   }
+
+//   deleteUser(id: string) {
+//     return this.userRepository.deleteUser(id);
+//   }
+// }
