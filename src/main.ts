3c7610c6d2c57e01504925eed267bf1c9954fb96
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-04-14 00:53:56 -0300

    modificado manin.ts

--- src/main.ts
+++ src/main.ts
@@ -39,9 +39,72 @@ async function bootstrap() {
   console.log('Zona horaria configurada:', process.env.TZ || 'No configurada');
   console.log('Hora local según el backend:', new Date().toLocaleString());
 
+  // Manejo de preflight requests CORS (Opciones)
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    '🚀 Servidor backend iniciado en el puerto:',
+    process.env.PORT ?? 3000,
+  );
 }
 bootstrap();
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import {
+//   loggerGlobal,
+//   LoggerMiddleware,
+// } from './middlewares/logger.middleware';
+// import { ValidationPipe } from '@nestjs/common';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   // Configuración de Swagger
+//   const SwaggerConfig = new DocumentBuilder()
+//     .setTitle('E-commerce')
+//     .setDescription('API para e-commerce')
+//     .setVersion('1.0')
+//     .addBearerAuth()
+//     .build();
+//   const document = SwaggerModule.createDocument(app, SwaggerConfig);
+//   SwaggerModule.setup('api', app, document);
+
+//   // Habilitar CORS con múltiples orígenes permitidos
+//   app.enableCors({
+//     origin: [
+//       'http://localhost:5173',
+//       'https://conlara.onrender.com',
+//       'https://conlara.com.ar',
+//     ], // Agrega todos los dominios permitidos
+//     methods: 'GET,POST,PUT,DELETE,OPTIONS',
+//     allowedHeaders: 'Content-Type, Authorization',
+//   });
+
+//   // Middleware y Validación Global
+//   app.use(loggerGlobal);
+//   const loggerMiddleware = new LoggerMiddleware();
+//   app.use(loggerMiddleware.use);
+//   app.useGlobalPipes(new ValidationPipe());
+//   console.log('Zona horaria configurada:', process.env.TZ || 'No configurada');
+//   console.log('Hora local según el backend:', new Date().toLocaleString());
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
 
 // import { NestFactory } from '@nestjs/core';
 // import { AppModule } from './app.module';
