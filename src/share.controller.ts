5d323b82a41f3b86d044abd1af85411b343ca726
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-04-14 00:16:33 -0300

    agregadoa proxy 2

--- src/share.controller.ts
+++ src/share.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { fetch } from 'undici';
+import { fetch } from 'undici'; // 👈 cambiamos node-fetch por undici
 
 @Controller('productos/share')
 export class ShareController {
@@ -8,29 +8,15 @@ export class ShareController {
   async proxyHtml(@Param('id') id: string, @Res() res: Response) {
     try {
       console.log(`Solicitud al proxy recibida con ID: ${id}`);
-
       const response = await fetch(
         `https://ecommerce-9558.onrender.com/products/share/${id}`,
-        {
-          method: 'GET',
-          headers: { Accept: 'text/html' },
-          timeout: 5000,
-        },
       );
-
-      if (!response.ok) {
-        throw new Error(`Error ${response.status}: ${response.statusText}`);
-      }
-
       const html = await response.text();
       res.setHeader('Content-Type', 'text/html');
-      res.setHeader('X-Proxy-Server', 'NestJS Proxy');
       res.send(html);
     } catch (err) {
       console.error('Error al obtener HTML remoto:', err);
-      res
-        .status(500)
-        .send(`Error al obtener el HTML del producto: ${err.message}`);
+      res.status(500).send('Error al obtener el HTML del producto.');
     }
   }
 }
